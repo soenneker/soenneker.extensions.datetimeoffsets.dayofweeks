3fcf6d71443cf340e721fca425f08eea7e2a8e79
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:52 -0500

    TUnit conversion

--- test/Soenneker.Extensions.DateTimeOffsets.DayOfWeeks.Tests/DateTimeOffsetsDayOfWeeksExtensionTests.cs
+++ test/Soenneker.Extensions.DateTimeOffsets.DayOfWeeks.Tests/DateTimeOffsetsDayOfWeeksExtensionTests.cs
@@ -1,11 +1,10 @@
-﻿using Soenneker.Tests.Unit;
-using Xunit;
+using Soenneker.Tests.Unit;
 
 namespace Soenneker.Extensions.DateTimeOffsets.DayOfWeeks.Tests;
 
 public sealed class DateTimeOffsetsDayOfWeeksExtensionTests : UnitTest
 {
-    [Fact]
+    [Test]
     public void Default()
     {
 
